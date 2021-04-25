ae7736468df7ea0304ce6706d28cea7c90656296
Yuli016 <ypantoja@csumb.edu>
2021-04-25 02:05:43 -0700

    Changed
    Made workout be able to change content without using another page

--- js/workout.js
+++ js/workout.js
@@ -1,6 +1,6 @@
 // source: https://codepen.io/k4u4u4u/pen/BJPYJP
 
-  var divs = ["beginner", "moderate", "intermediate", "challenge"];
+  var divs = ["beginner", "moderate", "intermediate", "challenge", "beginner2", "moderate2", "intermediate2", "challenge2", "beginner3", "moderate3", "intermediate3", "challenge3", "beginner4", "moderate4", "intermediate4", "challenge4"];
   var visibleDivId = null;
 
   function toggleVisibility(divId) {
